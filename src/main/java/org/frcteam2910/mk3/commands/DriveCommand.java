ee6d237f3f7feb8b1e9a025ebb918860bd7611ff
DriverStation2 <None>
2023-01-11 17:35:22 -0800

    initial update

--- src/main/java/org/frcteam2910/mk3/commands/DriveCommand.java
+++ src/main/java/org/frcteam2910/mk3/commands/DriveCommand.java
@@ -1,7 +1,9 @@
 package org.frcteam2910.mk3.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.frcteam2910.mk3.subsystems.DrivetrainSubsystem;
+import org.frcteam2910.mk3.Controls;
 import org.frcteam2910.common.math.Vector2;
 import org.frcteam2910.common.robot.input.Axis;
 
@@ -10,11 +12,15 @@ public class DriveCommand extends CommandBase {
     private final Axis forward;
     private final Axis strafe;
     private final Axis rotation;
+    private final boolean fieldOriented;
 
-    public DriveCommand(DrivetrainSubsystem drivetrain, Axis forward, Axis strafe, Axis rotation) {
+    private static boolean USE_JOYSTICK = false;
+
+    public DriveCommand(DrivetrainSubsystem drivetrain, Axis forward, Axis strafe, Axis rotation, boolean fieldOriented) {
         this.forward = forward;
         this.strafe = strafe;
         this.rotation = rotation;
+        this.fieldOriented = fieldOriented;
 
         drivetrainSubsystem = drivetrain;
 
@@ -23,12 +29,17 @@ public class DriveCommand extends CommandBase {
 
     @Override
     public void execute() {
-        drivetrainSubsystem.drive(new Vector2(-forward.get(false), strafe.get(false)), -rotation.get(false));
+        if (USE_JOYSTICK) {
+            //Joystick joystick = Controls.joystick;
+            //drivetrainSubsystem.drive(new Vector2(-joystick.getX(), joystick.getY()), Math.pow(joystick.getTwist(), 2)/3, fieldOriented);
+        } else {
+            drivetrainSubsystem.drive(new Vector2(-forward.get(false), strafe.get(false)), rotation.get(false), fieldOriented);
+        }
 //        if(drivetrainSubsystem.isFieldOriented()) System.out.println(drivetrainSubsystem.getGyroAngle());
     }
 
     @Override
     public void end(boolean interrupted) {
-        drivetrainSubsystem.drive(Vector2.ZERO, 0);
+        drivetrainSubsystem.drive(Vector2.ZERO, 0, true);
     }
 }
--- src/main/java/org/frcteam2910/mk3/subsystems/DrivetrainSubsystem.java
+++ src/main/java/org/frcteam2910/mk3/subsystems/DrivetrainSubsystem.java
@@ -1,24 +1,21 @@
 package org.frcteam2910.mk3.subsystems;
 
+import java.util.Map;
+
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.google.errorprone.annotations.concurrent.GuardedBy;
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj2.command.Subsystem;
-import org.frcteam2910.common.control.*;
-import org.frcteam2910.common.util.DrivetrainFeedforwardConstants;
-import org.frcteam2910.common.util.HolonomicFeedforward;
-import org.frcteam2910.mk3.Constants;
-import org.frcteam2910.common.drivers.Gyroscope;
+
+import org.frcteam2910.common.control.CentripetalAccelerationConstraint;
+import org.frcteam2910.common.control.HolonomicMotionProfiledTrajectoryFollower;
+import org.frcteam2910.common.control.MaxAccelerationConstraint;
+import org.frcteam2910.common.control.MaxVelocityConstraint;
+import org.frcteam2910.common.control.Path;
+import org.frcteam2910.common.control.PidConstants;
+import org.frcteam2910.common.control.Trajectory;
+import org.frcteam2910.common.control.TrajectoryConstraint;
 import org.frcteam2910.common.kinematics.ChassisVelocity;
 import org.frcteam2910.common.kinematics.SwerveKinematics;
 import org.frcteam2910.common.kinematics.SwerveOdometry;
@@ -27,8 +24,24 @@ import org.frcteam2910.common.math.Rotation2;
 import org.frcteam2910.common.math.Vector2;
 import org.frcteam2910.common.robot.UpdateManager;
 import org.frcteam2910.common.robot.drivers.Mk3SwerveModule;
-import org.frcteam2910.common.robot.drivers.Pigeon;
+import org.frcteam2910.common.util.DrivetrainFeedforwardConstants;
 import org.frcteam2910.common.util.HolonomicDriveSignal;
+import org.frcteam2910.common.util.HolonomicFeedforward;
+import org.frcteam2910.mk3.Constants;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
 
 public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  {
@@ -36,6 +49,9 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
     public static final double WHEELBASE = 17.5; // Inches
     public static final double STEER_GEAR_RATIO = (32.0 / 15.0) * (60.0 / 10.0);
     public static final double DRIVE_GEAR_RATIO = (50.0 / 14.0) * (19.0 / 25.0) * (45.0 / 15.0);
+    public static final double PEAK_OUTPUT = 1;
+    public static final double MIN_DRIVE_SPEED = 0.1;
+    public static final double MAX_DRIVE_SPEED = 1;
 
     private final Mk3SwerveModule[] modules;
 
@@ -45,11 +61,159 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
             new Vector2(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),        // Back left
             new Vector2(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0)        // Back right
     );
+
+    private final Field2d field2d = new Field2d();
     private boolean fieldOriented;
     private double driveSpeed;
 
-    public double getDriveSpeed() {
+    public DrivetrainSubsystem() {
+        register();
+        gyroscope = new AHRS(SPI.Port.kMXP);
+        synchronized (sensorLock) {
+            gyroscope.enableBoardlevelYawReset(true);
+        }
+        SmartDashboard.putData("Field", field2d);
+        enableFieldCentric();
+        toddlerMode();
+
+        follower = new HolonomicMotionProfiledTrajectoryFollower(FOLLOWER_TRANSLATION_CONSTANTS, FOLLOWER_ROTATION_CONSTANTS, FOLLOWER_FEEDFORWARD_CONSTANTS);
+
+        TalonFX frontLeftSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR);
+        TalonFX backLeftSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR);
+        TalonFX frontRightSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR);
+        TalonFX backRightSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR);
+
+        WPI_TalonFX frontLeftDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR);
+        WPI_TalonFX frontRightDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR);
+        WPI_TalonFX backLeftDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR);
+        WPI_TalonFX backRightDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR);
+        
+        this.frontLeftDriveMotor = frontLeftDriveMotor;
+        this.frontRightDriveMotor = frontRightDriveMotor;
+        this.backLeftDriveMotor = backLeftDriveMotor;
+        this.backRightDriveMotor = backRightDriveMotor;
+    
+        frontLeftSteeringMotor.setSelectedSensorPosition(0);
+        frontRightSteeringMotor.setSelectedSensorPosition(0);
+        backLeftSteeringMotor.setSelectedSensorPosition(0);
+        backRightSteeringMotor.setSelectedSensorPosition(0);
+
+
+       frontLeftDriveMotor.setInverted(true);
+       backRightDriveMotor.setInverted(true);
+
+
+        // Limit speed (testing only)
+        configTalon(frontLeftDriveMotor);
+        configTalon(frontRightDriveMotor);
+        configTalon(backLeftDriveMotor);
+        configTalon(backRightDriveMotor);
+
+        Mk3SwerveModule frontLeftModule = new Mk3SwerveModule(new Vector2(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                Constants.DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET,
+                STEER_GEAR_RATIO,
+                DRIVE_GEAR_RATIO,
+                frontLeftSteeringMotor,
+                frontLeftDriveMotor,
+                new CANCoder(Constants.DRIVETRAIN_FRONT_LEFT_ENCODER_PORT));
+
+        Mk3SwerveModule frontRightModule = new Mk3SwerveModule(new Vector2(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
+                Constants.DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET,
+                STEER_GEAR_RATIO,
+                DRIVE_GEAR_RATIO,
+                frontRightSteeringMotor,
+                frontRightDriveMotor,
+                new CANCoder(Constants.DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT));
+
+        Mk3SwerveModule backLeftModule = new Mk3SwerveModule(new Vector2(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
+                Constants.DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET,
+                STEER_GEAR_RATIO,
+                DRIVE_GEAR_RATIO,
+                backLeftSteeringMotor,
+                backLeftDriveMotor,
+                new CANCoder(Constants.DRIVETRAIN_BACK_LEFT_ENCODER_PORT));
+
+        Mk3SwerveModule backRightModule = new Mk3SwerveModule(new Vector2(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
+                Constants.DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET,
+                STEER_GEAR_RATIO,
+                DRIVE_GEAR_RATIO,
+                backRightSteeringMotor,
+                backRightDriveMotor,
+                new CANCoder(Constants.DRIVETRAIN_BACK_RIGHT_ENCODER_PORT));
+
+        modules = new Mk3SwerveModule[] {frontLeftModule, frontRightModule, backLeftModule, backRightModule};
+
+        moduleAngleEntries = new GenericEntry[modules.length];
+
+        ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+        odometryXEntry = tab.add("X", 0.0)
+                .withPosition(0, 0)
+                .withSize(1, 1)
+                .getEntry();
+        odometryYEntry = tab.add("Y", 0.0)
+                .withPosition(0, 1)
+                .withSize(1, 1)
+                .getEntry();
+        odometryAngleEntry = tab.add("Angle", 0.0)
+                .withPosition(0, 2)
+                .withSize(1, 1)
+                .getEntry();
+
+        ShuffleboardLayout[] moduleLayouts = {
+                tab.getLayout("Front Left Module", BuiltInLayouts.kList),
+                tab.getLayout("Front Right Module", BuiltInLayouts.kList),
+                tab.getLayout("Back Left Module", BuiltInLayouts.kList),
+                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+        };
+        for (int i = 0; i < modules.length; i++) {
+            ShuffleboardLayout layout = moduleLayouts[i]
+                    .withPosition(2 + i * 2, 0)
+                    .withSize(2, 4);
+            moduleAngleEntries[i] = layout.add("Angle", 0.0).getEntry();
+        }
+        tab.addNumber("Controller drive speed", this::getControllerDriveSpeed)
+                .withWidget(BuiltInWidgets.kTextView)
+                .withPosition(1, 0)
+                .withSize(1, 1);
+        driveSpeedEntry = tab.add("Drive speed", driveSpeed)
+                .withWidget(BuiltInWidgets.kNumberSlider)
+                .withProperties(Map.ofEntries(Map.entry("min", MIN_DRIVE_SPEED), Map.entry("max", MAX_DRIVE_SPEED)))
+                .withPosition(1, 1)
+                .withSize(1, 1)
+                .getEntry();
+        driveSpeedToggle = tab.add("Drive speed toggle", false)
+                .withWidget(BuiltInWidgets.kToggleButton)
+                .withPosition(1, 2)
+                .withSize(1, 1)
+                .getEntry();
+        tab.addNumber("Rotation Voltage", () -> {
+            HolonomicDriveSignal signal;
+            synchronized (stateLock) {
+                signal = driveSignal;
+            }
+
+            if (signal == null) {
+                return 0.0;
+            }
+
+            return signal.getRotation() * RobotController.getBatteryVoltage();
+        });
+    }
+
+    /**
+     * Returns the controller drive speed.
+     * @return The controller drive speed.
+     */
+    public double getControllerDriveSpeed() {
         return driveSpeed;
+    }
+
+    /**
+     * Returns either the Shuffleboard drive speed or the controller drive speed, based on the toggle.
+     * @return The drive speed to use.
+     */
+    public double getDriveSpeed() {
+        return driveSpeedToggle.getBoolean(false) ? driveSpeedEntry.getDouble(1) : driveSpeed;
     }
 
     public enum DriveSpeed{
@@ -64,6 +228,7 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
     }
     public void modifyDriveSpeed(double amt){
         driveSpeed+=amt;
+        driveSpeed = MathUtil.clamp(driveSpeed, MIN_DRIVE_SPEED, MAX_DRIVE_SPEED);
     }
 
     public void babyMode(){
@@ -119,12 +284,15 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
     private ChassisVelocity velocity;
 
     // Logging
-    private final NetworkTableEntry odometryXEntry;
-    private final NetworkTableEntry odometryYEntry;
-    private final NetworkTableEntry odometryAngleEntry;
+    private final GenericEntry odometryXEntry;
+    private final GenericEntry odometryYEntry;
+    private final GenericEntry odometryAngleEntry;
 
-    private final NetworkTableEntry[] moduleAngleEntries;
+    private final GenericEntry[] moduleAngleEntries;
 
+    private final GenericEntry driveSpeedEntry;
+    private final GenericEntry driveSpeedToggle;
+    
     private final WPI_TalonFX frontLeftDriveMotor;
     private final WPI_TalonFX frontRightDriveMotor;
     private final WPI_TalonFX backLeftDriveMotor;
@@ -138,133 +306,17 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
             new CentripetalAccelerationConstraint(6)
     };
 
-    public DrivetrainSubsystem() {
-        register();
-        gyroscope = new AHRS(SPI.Port.kMXP);
-        synchronized (sensorLock) {
-            gyroscope.enableBoardlevelYawReset(true);
-        }
-        enableFieldCentric();
-        toddlerMode();
-
-        follower = new HolonomicMotionProfiledTrajectoryFollower(FOLLOWER_TRANSLATION_CONSTANTS, FOLLOWER_ROTATION_CONSTANTS, FOLLOWER_FEEDFORWARD_CONSTANTS);
-
-        TalonFX frontLeftSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR);
-        TalonFX backLeftSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR);
-        TalonFX frontRightSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR);
-        TalonFX backRightSteeringMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR);
-
-        WPI_TalonFX frontLeftDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR);
-        WPI_TalonFX frontRightDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR);
-        WPI_TalonFX backLeftDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR);
-        WPI_TalonFX backRightDriveMotor = new WPI_TalonFX(Constants.DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR);
-        this.frontLeftDriveMotor = frontLeftDriveMotor;
-        this.frontRightDriveMotor = frontRightDriveMotor;
-        this.backLeftDriveMotor = backLeftDriveMotor;
-        this.backRightDriveMotor = backRightDriveMotor;
-    
-        frontLeftSteeringMotor.setSelectedSensorPosition(0);
-        frontRightSteeringMotor.setSelectedSensorPosition(0);
-        backLeftSteeringMotor.setSelectedSensorPosition(0);
-        backRightSteeringMotor.setSelectedSensorPosition(0);
-
-
-//        frontLeftDriveMotor.setInverted(true);
-//        backRightDriveMotor.setInverted(true);
-
-
-        // Limit speed (testing only)
-        configTalon(frontLeftDriveMotor);
-        configTalon(frontRightDriveMotor);
-        configTalon(backLeftDriveMotor);
-        configTalon(backRightDriveMotor);
-
-        Mk3SwerveModule frontLeftModule = new Mk3SwerveModule(new Vector2(TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-                Constants.DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET,
-                STEER_GEAR_RATIO,
-                DRIVE_GEAR_RATIO,
-                frontLeftSteeringMotor,
-                frontLeftDriveMotor,
-                null); //new CANCoder(Constants.DRIVETRAIN_FRONT_LEFT_ENCODER_PORT));
-
-        Mk3SwerveModule frontRightModule = new Mk3SwerveModule(new Vector2(TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
-                Constants.DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET,
-                STEER_GEAR_RATIO,
-                DRIVE_GEAR_RATIO,
-                frontRightSteeringMotor,
-                frontRightDriveMotor,
-                null); //new CANCoder(Constants.DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT));
-
-        Mk3SwerveModule backLeftModule = new Mk3SwerveModule(new Vector2(-TRACKWIDTH / 2.0, WHEELBASE / 2.0),
-                Constants.DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET,
-                STEER_GEAR_RATIO,
-                DRIVE_GEAR_RATIO,
-                backLeftSteeringMotor,
-                backLeftDriveMotor,
-                null); //new CANCoder(Constants.DRIVETRAIN_BACK_LEFT_ENCODER_PORT));
-
-        Mk3SwerveModule backRightModule = new Mk3SwerveModule(new Vector2(-TRACKWIDTH / 2.0, -WHEELBASE / 2.0),
-                Constants.DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET,
-                STEER_GEAR_RATIO,
-                DRIVE_GEAR_RATIO,
-                backRightSteeringMotor,
-                backRightDriveMotor,
-                null); //new CANCoder(Constants.DRIVETRAIN_BACK_RIGHT_ENCODER_PORT));
-
-        modules = new Mk3SwerveModule[] {frontLeftModule, frontRightModule, backLeftModule, backRightModule};
-
-        moduleAngleEntries = new NetworkTableEntry[modules.length];
-
-        ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
-        odometryXEntry = tab.add("X", 0.0)
-                .withPosition(0, 0)
-                .withSize(1, 1)
-                .getEntry();
-        odometryYEntry = tab.add("Y", 0.0)
-                .withPosition(0, 1)
-                .withSize(1, 1)
-                .getEntry();
-        odometryAngleEntry = tab.add("Angle", 0.0)
-                .withPosition(0, 2)
-                .withSize(1, 1)
-                .getEntry();
-
-        ShuffleboardLayout[] moduleLayouts = {
-                tab.getLayout("Front Left Module", BuiltInLayouts.kList),
-                tab.getLayout("Front Right Module", BuiltInLayouts.kList),
-                tab.getLayout("Back Left Module", BuiltInLayouts.kList),
-                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-        };
-        for (int i = 0; i < modules.length; i++) {
-            ShuffleboardLayout layout = moduleLayouts[i]
-                    .withPosition(2 + i * 2, 0)
-                    .withSize(2, 4);
-            moduleAngleEntries[i] = layout.add("Angle", 0.0).getEntry();
-        }
-        tab.addNumber("Rotation Voltage", () -> {
-            HolonomicDriveSignal signal;
-            synchronized (stateLock) {
-                signal = driveSignal;
-            }
-
-            if (signal == null) {
-                return 0.0;
-            }
-
-            return signal.getRotation() * RobotController.getBatteryVoltage();
-        });
-    }
-
+   
     public RigidTransform2 getPose() {
         synchronized (kinematicsLock) {
             return pose;
         }
     }
 
-    public void drive(Vector2 translationalVelocity, double rotationalVelocity) {
+    public void drive(Vector2 translationalVelocity, double rotationalVelocity, boolean fieldOriented) {
         if(Math.abs(rotationalVelocity) < 0.1) rotationalVelocity = 0;
         synchronized (stateLock) {
-            driveSignal = new HolonomicDriveSignal(translationalVelocity.scale(driveSpeed), Math.pow(rotationalVelocity*driveSpeed, 3), fieldOriented);
+            driveSignal = new HolonomicDriveSignal(translationalVelocity.scale(getDriveSpeed()), Math.pow(rotationalVelocity*getDriveSpeed(), 3), fieldOriented);
         }
     }
 
@@ -288,8 +340,8 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
     }
 
     private void configTalon(TalonFX talon) {
-        talon.configPeakOutputForward(0.3, 30);
-        talon.configPeakOutputReverse(-0.3, 30);
+        talon.configPeakOutputForward(PEAK_OUTPUT, 30);
+        talon.configPeakOutputReverse(-PEAK_OUTPUT, 30);
     }
 
     private void updateOdometry(double dt) {
@@ -306,6 +358,7 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
                 angle = Rotation2.fromDegrees(gyroscope.isMagnetometerCalibrated() ?
                         gyroscope.getFusedHeading() : 360.0 - gyroscope.getYaw());
         }
+        angle = Rotation2.fromDegrees(5);
 
         synchronized (kinematicsLock) {
             this.pose = swerveOdometry.update(angle, dt, moduleVelocities);
@@ -386,8 +439,17 @@ public class DrivetrainSubsystem implements Subsystem, UpdateManager.Updatable  
         for (int i = 0; i < modules.length; i++) {
             moduleAngleEntries[i].setDouble(Math.toDegrees(modules[i].getCurrentAngle()));
         }
+        field2d.setRobotPose(pose.translation.x,
+                             pose.translation.y,
+                             new Rotation2d(pose.rotation.toRadians()));
     }
+
+    public HolonomicMotionProfiledTrajectoryFollower getFollower() {
+        return follower;
+    }
+
     public void follow(Path p){
         follower.follow(new Trajectory(p, CONSTRAINTS, 12.0));
     }
+
 }
