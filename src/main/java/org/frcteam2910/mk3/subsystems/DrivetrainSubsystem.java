b7907cc7898fec628fc5d583e10dfcccb4a1cf47
jbko6 <58869582+jbko6@users.noreply.github.com>
2023-01-13 12:41:05 -0800

    modified coefficients, added gradlew for powershell

--- src/main/java/org/frcteam2910/mk3/subsystems/DrivetrainSubsystem.java
+++ src/main/java/org/frcteam2910/mk3/subsystems/DrivetrainSubsystem.java
@@ -3,6 +3,7 @@ package org.frcteam2910.mk3.subsystems;
 import org.frcteam2910.mk3.Constants;
 import org.opencv.calib3d.StereoBM;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
@@ -21,9 +22,9 @@ import org.frcteam2910.common.robot.UpdateManager;
 public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.Updatable {
 
     private final double ticksPerRotation = 2048.0;
-    private final double wheelDiameter = 3.0;
-    private final double driveReduction = 1.0;
-    private final double steerReduction = (32.0 / 15.0) * (60.0 / 10.0);
+    private final double wheelDiameterMeters = 0.0762; // 3 inches
+    private final double driveReductionL3 = (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 45.0); // verified
+    private final double steerReduction = (32.0 / 15.0) * (60.0 / 10.0); // verified
 
     private static final int TALONFX_PID_LOOP_NUMBER = 0;
     private static final double MAX_STEERING_SPEED = 0.5;
@@ -33,8 +34,8 @@ public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.
     // raw sensor unit = perimeter / 2048
 
     // units: raw sensor units
-    private final double steerPositionCoefficient = Math.PI * wheelDiameter * steerReduction / ticksPerRotation;
-    private final double driveVelocityCoefficient = (Math.PI * wheelDiameter * driveReduction / ticksPerRotation) * 10.0;
+    private final double steerPositionCoefficient = 2.0 * Math.PI * steerReduction / ticksPerRotation;
+    private final double driveVelocityCoefficient = (Math.PI * wheelDiameterMeters * driveReductionL3 / ticksPerRotation) * 10.0;
 
     WPI_TalonFX[] moduleDriveMotors = {
         new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR),
@@ -42,8 +43,6 @@ public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.
         new WPI_TalonFX(Constants.DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR),
         new WPI_TalonFX(Constants.DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR)
     };
-
-   // WPI_TalonFX motor1 = null;
 
     WPI_TalonFX[] moduleAngleMotors = {
         new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR),
@@ -80,12 +79,19 @@ public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.
 
     public DrivetrainSubsystem() {
         // configure encoders offsets
-        moduleEncoders[0].configMagnetOffset(moduleOffsets[0]);
-        moduleEncoders[1].configMagnetOffset(moduleOffsets[1]);
-        moduleEncoders[2].configMagnetOffset(moduleOffsets[2]);
-        moduleEncoders[3].configMagnetOffset(moduleOffsets[3]); 
+        for (int i = 0; i < moduleEncoders.length; i++) {
+            moduleEncoders[i].configMagnetOffset(moduleOffsets[i]);
+        }
 
-        for (int i = 0; i > moduleAngleMotors.length - 1; i++) {
+        // configure drive motors
+        for (int i = 0; i < moduleDriveMotors.length; i++) {
+            WPI_TalonFX driveMotor = moduleDriveMotors[i];
+            driveMotor.setNeutralMode(NeutralMode.Brake);
+            driveMotor.setSensorPhase(true);
+        }
+
+        // configure angle motors
+        for (int i = 0; i < moduleAngleMotors.length; i++) {
             WPI_TalonFX steeringMotor = moduleAngleMotors[i];
             steeringMotor.configFactoryDefault();
             steeringMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, TALONFX_PID_LOOP_NUMBER, Constants.CAN_TIMEOUT_MS);
@@ -100,9 +106,6 @@ public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.
             steeringMotor.configPeakOutputForward(MAX_STEERING_SPEED, Constants.CAN_TIMEOUT_MS);
             steeringMotor.configPeakOutputReverse(-MAX_STEERING_SPEED, Constants.CAN_TIMEOUT_MS);
         }
-
-        //motor1 = new WPI_TalonFX(Constants.DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR);
-        //System.out.println(motor1.getSupplyCurrent());
     }
 
     /**
@@ -132,11 +135,11 @@ public class DrivetrainSubsystem extends SubsystemBase implements UpdateManager.
         // Set motor speeds and angles
         for (int i=0; i < moduleDriveMotors.length; i++) {
             // meters/100ms * raw sensor units conversion
-            // moduleDriveMotors[i].set(TalonFXControlMode.Velocity, (states[i].speedMetersPerSecond / 10) * driveVelocityCoefficient);
+            moduleDriveMotors[i].set(TalonFXControlMode.Velocity, (states[i].speedMetersPerSecond / 10) * driveVelocityCoefficient);
             // System.out.println((states[i].speedMetersPerSecond / 10) * driveVelocityCoefficient);
         }
         for (int i=0; i < moduleAngleMotors.length; i++) {
-            moduleAngleMotors[i].set(TalonFXControlMode.Position, states[i].angle.getRadians() * steerPositionCoefficient * 40000); // steerpositioncoefficient is WRONG
+            moduleAngleMotors[i].set(TalonFXControlMode.Position, states[i].angle.getRadians() * steerPositionCoefficient * 40000); // steerpositioncoefficient is maybe fixed
             // moduleAngleMotors[i].set(TalonFXControlMode.Position, 1000);
             // System.out.println(states[i].angle.getRadians() * steerPositionCoefficient);
         }
